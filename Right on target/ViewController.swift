7ceb293ce884632ac00a9d30a02bf81cf45ede99
macbook <yevhen.biiak@icloud.com>
2022-05-01 21:13:49 +0300

    Main.storyboard v1

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel?
+    @IBOutlet var slider: UISlider?
+    @IBOutlet var button: UIButton?
+    @IBOutlet var progress: UIProgressView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
